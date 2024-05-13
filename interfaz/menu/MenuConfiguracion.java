95acd3c47851b2004065bd3034e2e8f517c2ed97
a23nicolasln <a23nicolasln@iessanclemente.net>
2024-05-13 11:56:01 +0200

    modificado el patron composite

--- interfaz/menu/MenuConfiguracion.java
+++ interfaz/menu/MenuConfiguracion.java
@@ -95,78 +95,159 @@ class MenuItem extends ComponenteMenu {
         super(nombre, sc);
     }
 
-
     /*
      * Método que ejecuta la acción asociada al item de menú
      */
     @Override
     void ejecutar() {
-        variables v = variables.getInstance();
-        listadoTareas lt = listadoTareas.getInstance();
 
-        switch (nombre) {
-            case "Agregar Tarea":
-                System.out.println("Ingrese la descripcion de la tarea");
-                String nombreTarea = sc.nextLine();
-                System.out.println("Ingrese la sala de la tarea");
-                String sala = sc.nextLine();
-                lt.addTarea(new tarea(nombreTarea, sala));
-                this.padre.ejecutar();
-                break;
-
-            case "Eliminar Tarea":
-                System.out.println("Tareas existentes:");
-                System.out.println(lt.VerTareas());
-                System.out.println();
-                System.out.println("Ingrese la descripcion de la tarea");
-                System.out.println(lt.removeTarea(sc.nextLine()));
-                this.padre.ejecutar();
-                break;
-
-            case "Ver Tareas":
-                System.out.println(lt.VerTareas());
-                this.padre.ejecutar();
-                break;
-
-            case "Agregar Jugador":
-                System.out.println("Escriba el nombre del jugador a agregar");
-                System.out.println(v.añadirJugador(sc.nextLine()));
-                this.padre.ejecutar();
-                break;
-
-            case "Eliminar Jugador":
-                System.out.println("Escriba el nombre del jugador a eliminar");
-                System.out.println(v.eliminarJugador(sc.nextLine()));
-                this.padre.ejecutar();
-                break;
-
-            case "Ver Jugadores":
-                System.out.println(v.printNombres());
-                this.padre.ejecutar();
-                break;
-
-            case "Modificar numero de impostores":
-                System.out.println("Numero de impostores actual: " + v.getCantidadImpostores());
-                System.out.println("Inserte el nuevo numero de impostores");
-                v.setCantidadImpostores(Integer.parseInt(sc.nextLine()));
-                this.padre.ejecutar();
-                break;
-
-            case "Modificar numero de tareas":
-                System.out.println("Numero de tareas actual: " + v.getCantidadTareas());
-                System.out.println("Inserte el nuevo numero de tareas");
-                v.setCantidadTareas(Integer.parseInt(sc.nextLine()));
-                this.padre.ejecutar();
-                break;
-
-            case "Jugar":
-                app app = new app();
-                app.estructuraJuego();
-                this.padre.ejecutar();
-
-        }
     }
 }
+
+class AgregarTarea extends MenuItem {
+
+    public AgregarTarea(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    @Override
+    void ejecutar() {
+        listadoTareas lt = listadoTareas.getInstance();
+        System.out.println("Ingrese la descripcion de la tarea");
+        String nombreTarea = sc.nextLine();
+        System.out.println("Ingrese la sala de la tarea");
+        String sala = sc.nextLine();
+        lt.addTarea(new tarea(nombreTarea, sala));
+        this.padre.ejecutar();
+    }
+
+}
+
+class EliminarTarea extends MenuItem {
+
+    public EliminarTarea(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    @Override
+    void ejecutar() {
+        listadoTareas lt = listadoTareas.getInstance();
+        System.out.println("Tareas existentes:");
+        System.out.println(lt.VerTareas());
+        System.out.println();
+        System.out.println("Ingrese la descripcion de la tarea");
+        System.out.println(lt.removeTarea(sc.nextLine()));
+        this.padre.ejecutar();
+    }
+
+}
+
+class VerTareas extends MenuItem {
+
+    public VerTareas(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    @Override
+    void ejecutar() {
+        listadoTareas lt = listadoTareas.getInstance();
+        System.out.println(lt.VerTareas());
+        this.padre.ejecutar();
+    }
+
+}
+
+class AgregarJugador extends MenuItem {
+
+    public AgregarJugador(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    @Override
+    void ejecutar() {
+        variables v = variables.getInstance();
+        System.out.println("Escriba el nombre del jugador a agregar");
+        System.out.println(v.añadirJugador(sc.nextLine()));
+        this.padre.ejecutar();
+    }
+
+}
+
+class EliminarJugador extends MenuItem {
+
+    public EliminarJugador(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    @Override
+    void ejecutar() {
+        variables v = variables.getInstance();
+        System.out.println("Escriba el nombre del jugador a eliminar");
+        System.out.println(v.eliminarJugador(sc.nextLine()));
+        this.padre.ejecutar();
+    }
+
+}
+
+class VerJugadores extends MenuItem {
+    variables v = variables.getInstance();
+
+    public VerJugadores(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    @Override
+    void ejecutar() {
+        System.out.println(v.printNombres());
+        this.padre.ejecutar();
+    }
+}
+
+class NumeroImpostores extends MenuItem {
+    variables v = variables.getInstance();
+
+    public NumeroImpostores(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    @Override
+    void ejecutar() {
+        System.out.println("Numero de impostores actual: " + v.getCantidadImpostores());
+        System.out.println("Inserte el nuevo numero de impostores");
+        v.setCantidadImpostores(Integer.parseInt(sc.nextLine()));
+        this.padre.ejecutar();
+    }
+}
+
+class NumeroTareas extends MenuItem {
+    variables v = variables.getInstance();
+
+    public NumeroTareas(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    @Override
+    void ejecutar() {
+        System.out.println("Numero de tareas actual: " + v.getCantidadTareas());
+        System.out.println("Inserte el nuevo numero de tareas");
+        v.setCantidadTareas(Integer.parseInt(sc.nextLine()));
+        this.padre.ejecutar();
+    }
+}
+
+class jugar extends MenuItem {
+
+    public jugar(String nombre, Scanner sc) {
+        super(nombre, sc);
+    }
+
+    void ejecutar() {
+        app app = new app();
+        app.estructuraJuego();
+        this.padre.ejecutar();
+    }
+}
+
 /*
  * Clase que representa el menú de configuración del juego. Este menú tiene
  * opciones para modificar las tareas y los jugadores del juego.
@@ -182,20 +263,20 @@ public class MenuConfiguracion {
 
         Menu Tareas = new Menu("Tareas", sc);
 
-        MenuItem AgregarTarea = new MenuItem("Agregar Tarea", sc);
-        MenuItem EliminarTarea = new MenuItem("Eliminar Tarea", sc);
-        MenuItem VerTareas = new MenuItem("Ver Tareas", sc);
+        MenuItem AgregarTarea = new AgregarTarea("Agregar Tarea", sc);
+        MenuItem EliminarTarea = new EliminarTarea("Eliminar Tarea", sc);
+        MenuItem VerTareas = new VerTareas("Ver Tareas", sc);
         Tareas.addMenu(AgregarTarea);
         Tareas.addMenu(EliminarTarea);
         Tareas.addMenu(VerTareas);
 
         Menu Jugadores = new Menu("Jugadores", sc);
 
-        MenuItem AgregarJugador = new MenuItem("Agregar Jugador", sc);
-        MenuItem EliminarJugador = new MenuItem("Eliminar Jugador", sc);
-        MenuItem VerJugadores = new MenuItem("Ver Jugadores", sc);
-        MenuItem modificarImpostores = new MenuItem("Modificar numero de impostores", sc);
-        MenuItem modificarTareas = new MenuItem("Modificar numero de tareas", sc);
+        MenuItem AgregarJugador = new AgregarJugador("Agregar Jugador", sc);
+        MenuItem EliminarJugador = new EliminarJugador("Eliminar Jugador", sc);
+        MenuItem VerJugadores = new VerJugadores("Ver Jugadores", sc);
+        MenuItem modificarImpostores = new NumeroImpostores("Modificar numero de impostores", sc);
+        MenuItem modificarTareas = new NumeroTareas("Modificar numero de tareas", sc);
         Jugadores.addMenu(AgregarJugador);
         Jugadores.addMenu(EliminarJugador);
         Jugadores.addMenu(VerJugadores);
@@ -206,7 +287,7 @@ public class MenuConfiguracion {
         configuracion.addMenu(Tareas);
         configuracion.addMenu(Jugadores);
 
-        MenuItem Jugar = new MenuItem("Jugar", sc);
+        MenuItem Jugar = new jugar("Jugar", sc);
 
         principal.addMenu(configuracion);
         principal.addMenu(Jugar);
---
+++ readme.md
@@ -0,0 +1,19 @@
+--Impresións ao escribir a App. ¿tiveches nalgún momento a sensación que algunha parte do código a estabas escribindo de forma non apropiada por falta de non aplicar un patrón non visto --ou por calquera outra cuestión?.
+
+Si no patron composite
+ parte de adivinar o impostor non puxen un limitador de tempo
+
+--Indica si precisañes empregar iteradores donde e porqué.
+
+Si para as rondas xa que creei un metodo que realiza unha ronda asi que con un iterador encargome que hasta que gañe alguen estas continuaran
+
+--Indica si precisañes sobreescribir hashcode/equals donde e porqué.
+
+No
+
+--Indica donde usaches comparable/comparator e porqué.
+
+Non o utilicei 
+
+--Unha lista breve das meioras achegadas por ti, e dicir, de funcións e restriccións que non se piden no enunciado.
+
